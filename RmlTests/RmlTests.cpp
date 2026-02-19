e00e3e0fd4af76336dfd7c33f0f1f1560eb1a495
bmarques1995 <brunosmarques1995@hotmail.com>
2026-02-19 17:17:30 -0300

    Adicionar arquivos de projeto.

---
+++ RmlTests/RmlTests.cpp
@@ -0,0 +1,12 @@
+﻿// RmlTests.cpp: define o ponto de entrada para o aplicativo.
+//
+
+#include "RmlTests.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
